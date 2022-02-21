ce0aa5c819b4c245339c6231fbce8ce56bdf8ed0
James Robertson <digital.robertson@gmail.com>
2022-02-21 15:42:46 +0000

    minor improvement: Replaced RXFHelper with RXFReader

--- lib/weblet.rb
+++ lib/weblet.rb
@@ -3,7 +3,7 @@
 # file: weblet.rb
 
 require 'rexle'
-require 'rxfhelper'
+require 'rxfreader'
 
 
 class WebletBuilder
@@ -11,19 +11,19 @@ class WebletBuilder
   def initialize(s, marker: nil, debug: false)
 
     @marker, @debug = marker, debug
-    
+
     s.strip!
-    
-    # the default marker is the hash symbol (#) but the client can set their 
+
+    # the default marker is the hash symbol (#) but the client can set their
     # own marker by simply using their custom symbol in the 1st line of input
     @marker ||= s[0]
-    
+
     a = scan(s.strip)
     puts 'a: ' + a.inspect if @debug
-    
+
     @a = build(a)
     @a.unshift *['weblet', {}, '']
-  
+
   end
 
   def to_a()
@@ -38,42 +38,42 @@ class WebletBuilder
     return a unless a.length > 1
 
     a.map {|x| scan(x, indent+1)}
-    
+
   end
 
   def build(a)
 
     puts 'a: ' + a.inspect if @debug
-    
+
     a.map do |x|
-      
+
       puts 'x: ' + x.inspect if @debug
-      
+
       if x.is_a? String then
-        
+
         head, body = x.split("\n", 2)
         puts 'head: ' + head.inspect if @debug
         puts 'body: ' + body.inspect if @debug
         puts 'marker: ' + @marker.inspect if @debug
-        
+
         [:node, {id: head[/#{@marker}(\w+)/,1]}, '',['![', {}, body.strip]]
-        
-      elsif x[0].is_a? String 
-        
+
+      elsif x[0].is_a? String
+
         head, body = x[0].split("\n", 2)
         puts 'body: ' + body.inspect if @debug
         puts 'marker: ' + @marker.inspect if @debug
-        
+
         [:node, {id: head[/#{@marker}(\w+)/,1]}, '',['![', {}, body.strip]]
-        
+
       elsif x[0] and x[0].is_a? Array
-                
+
         head, body = x[0][0].split("\n", 2)
         [:node, {id: head[/#{@marker}(\w+)/,1]}, '', \
          ['![', {}, body.to_s.strip], *build(x[1..-1])]
-        
+
       end
-      
+
     end
 
   end
@@ -83,10 +83,10 @@ end
 class Weblet
 
   def initialize(raws, marker: nil, debug: false)
-    
+
     @debug = debug
-    
-    s, _ = RXFHelper.read(raws.strip)
+
+    s, _ = RXFReader.read(raws.strip)
 
     obj = s[0] == '<' ? s : WebletBuilder.new(s, marker: marker, \
                                                     debug: debug).to_a
@@ -99,19 +99,19 @@ class Weblet
   def to_h()
     @h
   end
-  
+
   def to_outline()
     treeize(scan_h(@h))
   end
-  
+
   def to_xml()
     @doc.root.xml pretty: true
   end
 
   def render(*args)
-    
+
     b = args.pop if args.last.is_a? Binding
-    
+
     if args.first.is_a? String then
       path = args.first.split('/').map(&:to_sym)
     else
@@ -120,23 +120,23 @@ class Weblet
 
     #r = @h.dig *path
     r = digx(@h, path)
-    
+
     # check for interpolated substitution tags e.g/ <:card/svg/link>
     r.gsub!(/<:([^>]+)>/) {|x| self.render($1) } if r
-    
+
     puts 'r: ' + r.inspect if @debug
-    
+
     if r.nil? then
       found = @doc.root.element("//node[@id='#{path.join}']")
       r = found.cdatas.join if found
     end
-    
+
     eval('%Q(' + r + ')', b) if r
 
   end
 
   private
-  
+
   def digx(obj, a)
 
     h = obj.is_a?(Array) ? obj.last : obj
@@ -149,33 +149,33 @@ class Weblet
       r.is_a?(Array) ? r.first : r
     end
 
-  end  
+  end
 
   def scan(node)
 
     a = node.elements.map do |e|
-      
+
       puts 'e: ' + e.inspect if @debug
 
       nodes = e.xpath('node')
-      
+
       puts 'nodes: ' + nodes.inspect if @debug
-      
+
       r = if nodes.any? then
-      
+
         r2 = scan(e)
         e.cdatas.any? ? [e.cdatas.join(), r2] : r2
-        
+
       else
         e.cdatas.join()
       end
 
       [e.attributes[:id].to_sym, r]
-      
+
     end.to_h
-    
+
   end
-  
+
   def scan_h(h)
 
     a = h.map do |key, value|
@@ -183,7 +183,7 @@ class Weblet
     end
 
   end
-  
+
   def treeize(a, indent=-2)
 
     indent += 1
@@ -198,6 +198,6 @@ class Weblet
     end.join("\n")
 
   end
-  
-  
+
+
 end
--- weblet.gemspec
+++ weblet.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'weblet'
-  s.version = '0.3.5'
+  s.version = '0.4.0'
   s.summary = 'Intended for retrieving HTML templates from a ' +
       'convenient to use Hash-like object.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/weblet.rb']
-  s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.11')
-  s.add_runtime_dependency('rxfhelper', '~> 1.1', '>=1.1.3')
+  s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.14')
+  s.add_runtime_dependency('rxfreader', '~> 0.1', '>=0.1.2')
   s.signing_key = '../privatekeys/weblet.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
